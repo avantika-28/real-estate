0ea3d589a59767ced0d5ca69b10e9cef2e2465ac
Avantika Darekar <avantika18darekar@gmail.com>
2025-01-29 12:46:22 +0530

    created middlerware and error handler

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,5 @@
 import User from "../models/user.model.js";
-import bcryptjs from 'bcryptjs';
+import bcryptjs from "bcryptjs";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -7,8 +7,8 @@ export const signup = async (req, res, next) => {
   const newUser = new User({ username, email, password: hashedPassword });
   try {
     await newUser.save();
-    res.status(201).json('User created successfully!');
+    res.status(201).json("User created successfully!");
   } catch (error) {
-    res.status(500).json(error.message)
+    next(error);
   }
 };
