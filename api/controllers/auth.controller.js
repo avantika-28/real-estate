dd377a01e1aa8797976c44c776ba5c84dc6f4762
Avantika Darekar <avantika18darekar@gmail.com>
2025-01-29 14:31:16 +0530

    created signin api

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,8 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -8,6 +11,24 @@ export const signup = async (req, res, next) => {
   try {
     await newUser.save();
     res.status(201).json("User created successfully!");
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validateUser = await User.findOne({ email });
+    if (!validateUser) return next(errorHandler(404, "User Not Found"));
+    const validPassword = bcryptjs.compareSync(password, validateUser.password);
+    if (!validPassword) return next(errorHandler(404, "Wrong Credentials"));
+    const token = jwt.sign({ id: validateUser._id }, process.env.JWT_TOKEN);
+    const { password: pass, ...rest } = validateUser._doc;
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json(rest);
   } catch (error) {
     next(error);
   }
