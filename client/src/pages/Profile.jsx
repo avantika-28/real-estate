ea3cfbdce48dbec5cf87fe45e7a38535d8004720
Avantika Darekar <avantika18darekar@gmail.com>
2025-01-31 12:38:24 +0530

    added ui of profile

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,7 +1,42 @@
-
+import { useSelector } from "react-redux";
 
 export default function Profile() {
+  const { curentUser } = useSelector((state) => state.user);
   return (
-    <div>Profile</div>
-  )
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl font-semibold text-center my-7 ">Profile</h1>
+      <form className="flex flex-col gap-4">
+        <img
+          src={curentUser.avatar}
+          className="rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2"
+          alt="profile"
+        />
+        <input
+          type="text"
+          id="username"
+          placeholder="Username"
+          className="border p-3 rounded-lg border-slate-300"
+        />
+        <input
+          type="email"
+          id="email"
+          placeholder="Email"
+          className="border p-3 rounded-lg border-slate-300"
+        />
+        <input
+          type="password"
+          id="password"
+          placeholder="Password"
+          className="border p-3 rounded-lg border-slate-300"
+        />
+        <button className="bg-slate-700 text-white p-3 rounded-lg hover:opacity-95 disabled:opacity-80 cursor-pointer">
+          Update
+        </button>
+      </form>
+      <div className="flex justify-between mt-4">
+        <span className="text-red-700 cursor-pointer">Delete Account</span>
+        <span className="text-red-700 cursor-pointer">Sign Out</span>
+      </div>
+    </div>
+  );
 }
