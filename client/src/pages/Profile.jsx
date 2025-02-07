2687474abffbff6e6a17944d913a76ba97687c6e
Avantika Darekar <avantika18darekar@gmail.com>
2025-02-07 12:14:51 +0530

    create user update api

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,12 +1,25 @@
+import { useEffect, useRef, useState } from "react";
 import { useSelector } from "react-redux";
 
 export default function Profile() {
   const { curentUser } = useSelector((state) => state.user);
+  // const fileRef = useRef(null);
+  // const [file, setFile] = useState(undefined);
+  // useEffect(() => {}, [file]);
+  console.log(curentUser.avatar);
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7 ">Profile</h1>
       <form className="flex flex-col gap-4">
+        {/* <input
+          onChange={(e) => setFile(e.target.files[0])}
+          type="file"
+          ref={fileRef}
+          accept="image/*"
+          hidden
+        /> */}
         <img
+          // onClick={() => fileRef.current.click()}
           src={curentUser.avatar}
           className="rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2"
           alt="profile"
@@ -29,7 +42,7 @@ export default function Profile() {
           placeholder="Password"
           className="border p-3 rounded-lg border-slate-300"
         />
-        <button className="bg-slate-700 text-white p-3 rounded-lg hover:opacity-95 disabled:opacity-80 cursor-pointer">
+        <button className="bg-slate-700 text-white p-3 rounded-lg hover:opacity-95 disabled:opacity-80 cursor-pointer uppercase">
           Update
         </button>
       </form>
